113965cbb5f2dbbf1939441e1f56c45efb706a39
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-11-01 17:17:07 +0100

    feat: Add homeContainerReducer to the root reducer
    - Imported homeContainerReducer and included it in the combined reducers.
    - This allows for managing the home container state within the Redux store.

--- src/reducers.js
+++ src/reducers.js
@@ -6,6 +6,8 @@ import { combineReducers } from 'redux';
 // import languageProviderReducer from './containers/LanguageProvider/reducer';
 // import globalReducer from './containers/App/reducer';
 
+import homeContainerReducer from './containers/HomeContainer/reducer';
+
 /**
  * Merges the main reducer with the router state and dynamically injected reducers
  */
@@ -13,6 +15,7 @@ export default function createReducer(injectedReducers = {}) {
   const rootReducer = combineReducers({
     // language: languageProviderReducer,
     // global: globalReducer,
+    homeContainer: homeContainerReducer,
     ...injectedReducers,
   });
 
