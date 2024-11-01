67a1987042c5836b34a4c5cd411bbd2bd636db84
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-11-01 17:17:44 +0100

    Add decrement action to HomeContainer
    - Imported decrementAction from actions.
    - Updated mapDispatchToProps to include onDecrement for handling decrement actions.

--- src/containers/HomeContainer/index.js
+++ src/containers/HomeContainer/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'; 
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { createStructuredSelector } from 'reselect';
 
 import Home from '../../views/Home';
 import { makeSelectHomeContainerCounter } from './selector';
-import { incrementAction } from './action';
+import { incrementAction, decrementAction } from './action';
 import { useInjectReducer } from '../../utils/injectReducer';
 
 import reducer from './reducer';
@@ -26,6 +26,7 @@ export function mapDispatchToProps(dispatch) {
   return {
     dispatch,
     onIncrement: () => dispatch(incrementAction()),
+    onDecrement: () => dispatch(decrementAction()),
   };
 }
 
@@ -37,4 +38,3 @@ const withConnect = connect(
 export default compose(
   withConnect,
 )(HomeContainer);
-
