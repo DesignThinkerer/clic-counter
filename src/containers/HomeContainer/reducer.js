41b61973e687edce1f9bff8d9992c1000d2b9c05
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-11-01 17:07:51 +0100

    feat(HomeContainer): update reducer to handle decrement action
    - Imported DECREMENT constant from './constant'
    - Added case for DECREMENT in homeContainerReducer to decrease counter without going below zero

--- src/containers/HomeContainer/reducer.js
+++ src/containers/HomeContainer/reducer.js
@@ -1,16 +1,21 @@
-import { INCREMENT } from './constant';
+import { INCREMENT, DECREMENT } from './constant';
 
 export const initialState = {
   counter: 0,
 };
 
-export default function homeContainerReducer(state = initialState, action) {
+export default function homeContainerReducer(state = initialState, action){
   switch (action.type) {
     case INCREMENT:
       return {
         ...state,
         counter: state.counter + 1,
       };
+      case DECREMENT:
+        return {
+          ...state,
+          counter: Math.max(state.counter - 1, 0), // Le compteur ne descend pas en dessous de 0
+        }
     default:
       return state;
   }
