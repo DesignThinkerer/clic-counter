087c9fbcdf9ab58f727f365403333db42a73aa55
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-11-01 18:26:37 +0100

    fix: use HashRouter for routing compatibility on GitHub Pages
    Replaced BrowserRouter with HashRouter in App.js to prevent 404 errors on page refresh or direct navigation in the GitHub Pages environment. This change ensures that all routes are handled correctly.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter  as Router, Switch, Route } from 'react-router-dom';
 import configureStore from './configureStore';
 import history from './utils/history';
 import Home from './containers/HomeContainer';
